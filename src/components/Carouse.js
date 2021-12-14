588aa32c0260e67d23e25bfe50a6667f4bc6365a
Lruler <1204312199@qq.com>
2021-12-14 18:08:55 +0800

    feat: 抽出轮播图图片样式函数

--- src/components/Carouse.js
+++ src/components/Carouse.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Swiper from 'react-native-swiper';
 
 const Carouse = props => {
   const {images} = props;
+  const imgStyle = image => {
+    let styles = StyleSheet.create({
+      height: '100%',
+      width: '100%',
+      backgroundColor: `${image}`,
+    });
+    return styles;
+  };
   return (
     <Swiper horizontal={true} showsButtons={true}>
       {images.map(image => {
-        return (
-          <View
-            style={{backgroundColor: `${image}`, height: '100%', width: '100%'}}
-          />
-        );
+        return <View style={imgStyle(image)} />;
       })}
     </Swiper>
   );
