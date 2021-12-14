65f39ba4df26880bf3e8c87ef521a5e1c0ecd459
Lruler <1204312199@qq.com>
2021-12-14 18:32:20 +0800

    fix: delete useLess import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 import footImg1 from '../static/1.png';
 import footImg2 from '../static/2.png';
 import footImg3 from '../static/3.png';
